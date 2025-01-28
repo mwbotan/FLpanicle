36efa9a2206a61eb60451ff0c121e82625a20a36
mwbotan <166492792+mwbotan@users.noreply.github.com>
2025-01-28 21:40:17 +0900

    Update train_split.py

--- train_split.py
+++ train_split.py
@@ -12,7 +12,7 @@ def get_exif(image_path):
     captured_datetime = d.strftime("%Y%m%d_%H%M%S")
     return captured_datetime
 
-files = glob.glob("./timelapse/train/*")
+files = glob.glob("./train/*")
 
 for file in files:
     fname = get_exif(file)
@@ -23,6 +23,6 @@ for file in files:
         for j in range(4):
             count = count+1
             img2 = imgL[(700*i):(700*(i+1)),(700*j):(700*(j+1))]
-            cv2.imwrite(f'../../timelapse/train_split/{fname}_{count}.jpg',img2)
+            cv2.imwrite(f'./train_split/{fname}_{count}.jpg',img2)
     
 
